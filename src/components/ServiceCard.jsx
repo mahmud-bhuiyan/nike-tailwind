3004c9a44d3f8ba4dcc4cc7f7592e054df872b08
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-09-08 18:06:28 +0600

    bug fixed

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,6 +1,6 @@
 const ServiceCard = ({ service }) => {
   const { imgURL, label, subtext } = service;
-  console.log(service);
+
   return (
     <div className="flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16">
       <div className="w-11 h-11 flex justify-center items-center bg-coral-red rounded-full">
